1789241b62e3054fd9ae3ce5c2d18286e53a15ce
Programmer Hrishav <programmerhrishav1234@gmail.com>
2019-10-05 16:32:24 +0530

    Principal Dashboard - pending request : 100%

--- src/com/attendance/dashboard/PrincipalDashboardController.java
+++ src/com/attendance/dashboard/PrincipalDashboardController.java
@@ -7,6 +7,7 @@ package com.attendance.dashboard;
 
 import com.attendance.login.activity.dao.Activity;
 import com.attendance.login.activity.model.LoginActivity;
+import com.attendance.login.dao.Login;
 import com.attendance.login.user.model.User;
 import com.attendance.main.Start;
 import com.attendance.util.Fxml;
@@ -24,6 +25,8 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
+import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -43,102 +46,124 @@ import javafx.stage.StageStyle;
  * @author pc
  */
 public class PrincipalDashboardController extends AnchorPane {
-    
+
     @FXML
     private JFXButton student;
-    
+
     @FXML
     private JFXButton faculty;
-    
+
     @FXML
     private JFXButton classdetails;
-    
+
     @FXML
     private JFXButton dailyclassreport;
-    
+
     @FXML
     private JFXButton settings;
-    
+
     @FXML
     private JFXButton myprofile;
-    
+
     @FXML
     private JFXButton changepassword;
-    
+
     @FXML
     private JFXButton logout;
-    
+
     @FXML
     private JFXComboBox<String> department;
-    
+
     @FXML
     private JFXComboBox<String> year;
-    
+
     @FXML
     private JFXButton open;
-    
+
     @FXML
     private JFXButton refresh;
-    
+
     @FXML
     private VBox list;
-    
+
     @FXML
     private JFXButton totalstudents;
-    
+
     @FXML
     private JFXButton totalfaculties;
-    
+
     @FXML
     private JFXButton totaldepartments;
-    
+
     @FXML
     private JFXButton principalaccounts;
-    
+
     @FXML
     private JFXButton hodaccounts;
-    
+
     @FXML
     private AnchorPane pane;
-    
+
     @FXML
     private JFXButton verifyhod;
-    
+
     @FXML
     private Label pending;
-    
+
     @FXML
-    private ImageView profilepic;    
-    
+    private ImageView profilepic;
+
     private User principal;
     private LoginActivity activity;
     private FXMLLoader fxml;
     private Activity act;
-    
+    private Login login;
+
+    private Task<Integer> task;
+    private Thread thread;
+
     public PrincipalDashboardController() {
         this.principal = SystemUtils.getCurrentUser();
         this.activity = SystemUtils.getActivity();
-        
+
         fxml = Fxml.getPrincipalDashboardFXML();
         fxml.setController(this);
         fxml.setRoot(this);
-        
+
         try {
             fxml.load();
         } catch (IOException ex) {
             Logger.getLogger(PrincipalDashboardController.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
     }
-    
+
     @FXML
     private void initialize() {
         act = (Activity) Start.app.getBean("loginactivity");
+        login = (Login) Start.app.getBean("userlogin");
         buttonActions();
         profilepic.setImage(new Image(new ByteArrayInputStream(principal.getImage())));
         initLoginActivity(null);
+        taskInit();
+        thread.start();
     }
-    
+
+    private void taskInit() {
+        task = new Task<Integer>() {
+            @Override
+            protected Integer call() throws Exception {
+                while (true) {
+                    Platform.runLater(() -> PrincipalDashboardController.this.init());
+
+                    Thread.sleep(2000);
+                }
+
+            }
+        };
+        thread = new Thread(task);
+    }
+
     private void buttonActions() {
         refresh.setOnAction(this::initLoginActivity);
         myprofile.setOnAction(e -> SwitchRoot.switchRoot(Start.st, RootFactory.getUserProfileRoot("principal")));
@@ -154,9 +179,15 @@ public class PrincipalDashboardController extends AnchorPane {
         totaldepartments.setOnAction(e -> pane.getChildren().setAll(RootFactory.getPrincipalDashboardDepartmentNodeRoot()));
         principalaccounts.setOnAction(e -> pane.getChildren().setAll(RootFactory.getPrincipalDashboardPrincipalAccountNodeRoot()));
         hodaccounts.setOnAction(e -> pane.getChildren().setAll(RootFactory.getPrincipalDashboardHODAccountNodeRoot()));
-        
+
     }
-    
+
+    private void init() {
+        List<User> collected = login.findByStatus("Pending");
+        collected = collected.stream().filter(p -> p.getType().equals("HOD")).collect(Collectors.toList());
+        pending.setText("" + collected.size());
+    }
+
     private void initLoginActivity(ActionEvent e) {
         List<LoginActivity> allLogins = new ArrayList<>(act.findByDepartment("N/A"));
         allLogins = allLogins.stream().filter(p -> p.getUserType().equals("Principal")).collect(Collectors.toList());
@@ -164,15 +195,15 @@ public class PrincipalDashboardController extends AnchorPane {
         List<PrincipalLoginActivityController> activityControllers = allLogins.stream().map(c -> new PrincipalLoginActivityController(c)).collect(Collectors.toList());
         list.getChildren().setAll(activityControllers);
     }
-    
+
     private class ShowDepartmentPage implements EventHandler<ActionEvent> {
-        
+
         private final String type;
-        
+
         public ShowDepartmentPage(String type) {
             this.type = type;
         }
-        
+
         @Override
         public void handle(ActionEvent t) {
             Stage st = StageUtil.newStage().centerOnScreen().fullScreen(false).fullScreenExitHint("").fullScreenExitKeyCombination(null)
@@ -180,23 +211,22 @@ public class PrincipalDashboardController extends AnchorPane {
                     .build();
             if (type.equals("Student")) {
                 st.getScene().setRoot(RootFactory.getSelectDepartmentRoot("Student", PrincipalDashboardController.this.getScene().getRoot()));
-                
+
             } else if (type.equals("Faculty")) {
                 st.getScene().setRoot(RootFactory.getSelectDepartmentRoot("Faculty", PrincipalDashboardController.this.getScene().getRoot()));
-                
+
             } else if (type.equals("Class Details")) {
                 st.getScene().setRoot(RootFactory.getSelectDepartmentRoot("Class Details", PrincipalDashboardController.this.getScene().getRoot()));
-                
+
             } else if (type.equals("Daily Class Details")) {
                 st.getScene().setRoot(RootFactory.getSelectDepartmentRoot("Daily Class Details", PrincipalDashboardController.this.getScene().getRoot()));
-                
-            }
-            else if (type.equals("verifyhod")) {
+
+            } else if (type.equals("verifyhod")) {
                 st.getScene().setRoot(RootFactory.getSelectDepartmentRoot("verifyhod", PrincipalDashboardController.this.getScene().getRoot()));
-                
+
             }
             st.show();
         }
     }
-    
+
 }
