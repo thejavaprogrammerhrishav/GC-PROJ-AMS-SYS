4ab74e5c4829c53f9790b6c7c7b0d50f494ed9f8
thejavaprogrammerhrishav <38313386+thejavaprogrammerhrishav@users.noreply.github.com>
2019-12-25 11:49:05 +0530

    delete routine

--- src/com/attendance/routines/controller/ViewAllRoutineNodeController.java
+++ src/com/attendance/routines/controller/ViewAllRoutineNodeController.java
@@ -5,12 +5,15 @@
  */
 package com.attendance.routines.controller;
 
+import com.attendance.main.Start;
+import com.attendance.routines.dao.RoutineDao;
 import com.attendance.routines.model.Routine;
 import com.attendance.util.Fxml;
 import com.jfoenix.controls.JFXButton;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
@@ -30,11 +33,16 @@ public class ViewAllRoutineNodeController extends AnchorPane{
 
     @FXML
     private JFXButton view;
+    
+    @FXML
+    private JFXButton delete;
 
     
     private FXMLLoader fxml;
     
     private Routine routine;
+    
+    private RoutineDao dao;
 
     public ViewAllRoutineNodeController(Routine routine) {
         this.routine = routine;
@@ -50,10 +58,18 @@ public class ViewAllRoutineNodeController extends AnchorPane{
     
     @FXML
     private void initialize() {
+        
+        dao = (RoutineDao) Start.app.getBean("routine");
         name.setText(routine.getFilename());
         date.setText(routine.getDate());
         
         view.setOnAction(e-> ViewRoutineController.show(routine, this.getScene()));
+        delete.setOnAction(this::delete);
+    }
+    
+    
+    private void delete(ActionEvent evt) {
+        dao.delete(routine);
     }
     
 }
